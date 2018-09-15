9e9aebb7a46cae3198c881cc56482088e42f97ce
tomoyoshi-0104 <38575492+tomoyoshi-0104@users.noreply.github.com>
2018-09-15 22:39:19 +0900

    02.数値、03.日付・時間

---
+++ 基礎編/02.数値/sample02.py
@@ -0,0 +1,24 @@
+#四則演算
+test_integer = 100
+
+print(test_integer + 10)
+print(test_integer - 10)
+print(test_integer * 10)
+print(test_integer / 10)
+
+#数値へ変換
+test_str = '100'
+print(int(test_str) + 100)
+
+#浮動小数点数
+test_str = '100.5'
+print(float(test_str) + 100)
+
+test_float = .5
+print(test_float)
+
+#複素数
+test_complex = 100 + 5j
+print(test_complex)
+print(test_complex.real)
+print(test_complex.imag)
