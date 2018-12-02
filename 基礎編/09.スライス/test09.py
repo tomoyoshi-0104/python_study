43ba5e11ef415e3d651f079cba7b2cc58ae2751c
tomoyoshi-0104 <38575492+tomoyoshi-0104@users.noreply.github.com>
2018-12-02 20:10:05 +0900

    09.スライス～11.インポート

---
+++ 基礎編/09.スライス/test09.py
@@ -0,0 +1,24 @@
+#スライスの基本
+test_list = ['https', 'www', 'python', 'izm', 'com']
+
+print(test_list[:])
+print(test_list[::])
+
+#要素の取得
+print(test_list[:4])
+
+print(test_list[2:])
+
+print(test_list[::2])
+
+print(test_list[3:5])
+
+print(test_list[-1:])   # 末尾から全ての要素
+print(test_list[:-1])   # 末尾まで全ての要素
+print(test_list[::-1])  # 末尾から全ての逆順要素
+
+print(test_list[:999])
+
+#要素の代入
+test_list[1:3] = ('WWW', 'PYTHON')
+print(test_list)
