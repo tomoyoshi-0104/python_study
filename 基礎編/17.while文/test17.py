b4b3f98c26e38b85cf76cee91393159213fe171d
tomoyoshi-0104 <38575492+tomoyoshi-0104@users.noreply.github.com>
2020-09-11 21:39:16 +0900

    17.while文～20.range

---
+++ 基礎編/17.while文/test17.py
@@ -0,0 +1,13 @@
+counter = 0
+
+while counter < 10:
+    counter += 1
+    print(counter)
+
+counter = 0
+
+while True:
+    counter += 1
+    print(counter)
+    if counter ==10:
+        break
---
+++ 基礎編/20.rangeとxrange/test20.py
@@ -0,0 +1,11 @@
+for i in range(10):
+    print(i)
+print()
+for i in range(2,10):
+    print(i)
+print()
+for i in range(-2,10):
+    print(i)
+print("xrangeはpython3にて廃止")
+# for i in xrange(10):
+#     print(i)
